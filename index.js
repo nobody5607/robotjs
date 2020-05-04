4587d75e9345c81098d015702ba0afd306fa9629
NuttaphonChanpan <chanpan.nuttaphon@gmail.com>
2020-05-04 18:05:57 +0700

    robotjs

---
+++ index.js
@@ -0,0 +1,10 @@
+// Type "Hello World" then press enter.
+var robot = require("robotjs");
+
+// Type "Hello World".
+// robot.typeString("Hello World");
+robot.typeString("abc123");
+
+
+// Press enter.
+// robot.keyTap("enter");
